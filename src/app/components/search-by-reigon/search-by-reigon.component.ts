7bb4bf106655fb23f176c407ee1d9ede1c36c2bc
Deema Hafiz <hafezdema1999@gmail.com>
2020-01-26 19:39:59 +0200

    using vewchild decorator to apply shrinking header on scroll

--- src/app/components/search-by-reigon/search-by-reigon.component.ts
+++ src/app/components/search-by-reigon/search-by-reigon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { WeatherService } from './../../services/weather.service';
 
 @Component({
@@ -11,24 +11,29 @@ export class SearchByReigonComponent implements OnInit {
   lng: number;
   lat: number;
   name: string = "";
-  count : number = 50;
+  count: number = 50;
   public regions = {};
- 
+  position: number;
 
   constructor(private currentWeather : WeatherService) { }
   
-  
+  @ViewChild('search',{static :false}) search: ElementRef;
+
   @HostListener('window:scroll', ['$event']) getScrollHeight(event) {
-    console.log(window.pageYOffset);
-    let scrollPosition = Math.round(window.pageYOffset)
     
-    if (scrollPosition > 100){
+    let scrollPosition = Math.round(window.pageYOffset)
+    if (scrollPosition > this.position){
       document.querySelector('header').classList.add('sticky');
     }
     else {
       document.querySelector('header').classList.remove('sticky');
     }
   }
+
+  ngAfterViewInit() {
+    this.position  = this.search.nativeElement.offsetTop;
+  }
+
   ngOnInit() {
 
     if (navigator) {
@@ -41,7 +46,7 @@ export class SearchByReigonComponent implements OnInit {
       
     
   }
-  //@ViewChild('search',{static :false})
+   
 
   changeRout() {
     this.currentWeather.setRout(false)
